cf892d124bb3fac9d760f03b2af41478d142b3c2
Jubaidul Alam <jubaidul.ctg.bd@gmail.com>
2022-11-24 14:48:04 +0600

    Search box font size update

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <div v-if="!isMobile" class="hidden lg:block relative homeBanner">
       <div class="absolute lg:top-[70px] xl:top-[80px] 2xl:top-[150px] lg:bottom-[100px] xl:bottom-[130px] 2xl:bottom-[170px] lg:right-[80px] xl:right-[100px] lg:left-[80px] xl:left-[100px] z-40">
         <div class="">
-          <h2 class="lg:text-[44px] xl:text-[56px] text-[#fff] font-bold leading-[64px] ">Book Your Journey NOW!!!</h2>
+          <h2 class="lg:text-[44px] xl:text-[56px] 2xl:text-[57px] text-[#fff] font-bold leading-[64px] ">Book Your Journey NOW!!!</h2>
           <h4 class="mt-4 lg:text-lg xl:text-xl text-white leading-7 font-bold">Buy tickets online hassle-free</h4>
           <div class="mt-9">
             <SearchFilterForm/>
