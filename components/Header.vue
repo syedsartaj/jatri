e3d752fa2fb9080f10584a619e1609429ad798cc
Rijon <rijon.jatri@gmail.com>
2022-12-15 15:43:21 +0600

    [FE][t/861m3hz9c] Home page
    1. Add full bus reservation button in header
    2. Contact button update in header
    3. Hamburger menu added for mobile devie

--- components/Header.vue
+++ components/Header.vue
@@ -1,24 +1,99 @@
 <template>
-     <div class="header">
-          <NuxtLink to="/"><img src="@/assets/images/logo.svg" alt="logo" class="h-10 w-[79]"/></NuxtLink>
+  <div class="header">
+    <NuxtLink to="/"
+      ><img src="@/assets/images/logo.svg" alt="logo" class="h-10 w-[79]"
+    /></NuxtLink>
 
-          <div class="flex gap-x-8">
-               <a href="tel:09642080808" class="bg-[#f7f7f7] text-blackPrimary rounded-full px-4 py-2 justify-between gap-x-[10.5px] hidden lg:flex">
-                    <img src="@/assets/images/icons/phoneBlack.svg" alt=""> 
-                    <span>09642080808</span>
-               </a>
+    <div class="flex gap-x-4 h-10">
+      <a
+        href="tel:09642080808"
+        class="
+          border-[1px] border-[#DBDBDB]
+          text-blackPrimary
+          rounded-full
+          px-4
+          py-2
+          justify-between
+          gap-x-[10.5px]
+          hidden
+          lg:flex
+        "
+      >
+        <img
+          class="h-5 w-5"
+          src="@/assets/images/icons/phoneBlack.svg"
+          alt=""
+        />
+        <span>09642080808</span>
+      </a>
 
-               <NuxtLink to="/find-ticket" class="bg-[#FEF2F0] text-corporate rounded-full px-4 py-2 flex justify-between gap-x-[10.5px]"> 
-                    <img src="@/assets/images/icons/searchIcon.svg" alt=""> 
-                    <span>Ticket History</span>
-               </NuxtLink>
-          </div>
-     </div>
+      <NuxtLink
+        to="/find-ticket"
+        class="
+          bg-[#f7f7f7]
+          text-blackPrimary
+          rounded-full
+          px-4
+          py-2
+          justify-between
+          gap-x-[10.5px]
+          hidden
+          lg:flex
+        "
+      >
+        <img src="@/assets/images/home/fullBusReserve.svg" alt="" />
+        <span class="w-32">Full bus reserve</span>
+      </NuxtLink>
+
+      <NuxtLink
+        to="/find-ticket"
+        class="
+          bg-[#FEF2F0]
+          text-corporate
+          rounded-full
+          px-4
+          py-2
+          flex
+          justify-between
+          gap-x-[10.5px]
+        "
+      >
+        <img src="@/assets/images/icons/searchIcon.svg" alt="" />
+        <span class="w-15">Find ticket</span>
+      </NuxtLink>
+      <div class="flex lg:hidden">
+        <div v-if="showHamburgerMenu">
+          <HamburgerMenu :close="handleHamburgerMenu" />
+        </div>
+        <div v-else class="py-2">
+          <img
+            class="h-6 w-6 cursor-pointer"
+            @click="handleHamburgerMenu"
+            src="@/assets/images/home/hamburger.svg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import HamburgerMenu from "../components/Modal/HamburgerMenu.vue";
+export default {
+  components: { HamburgerMenu },
+  data() {
+    return {
+      showHamburgerMenu: false,
+    };
+  },
+  methods: {
+    handleHamburgerMenu() {
+      this.showHamburgerMenu = !this.showHamburgerMenu;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
