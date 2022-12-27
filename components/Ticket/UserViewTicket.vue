67b6abef08ab4c115418eb7ad17d70b4736aeafe
Rijon <rijon.jatri@gmail.com>
2022-12-27 14:17:47 +0600

    Restore print issue

--- components/Ticket/UserViewTicket.vue
+++ components/Ticket/UserViewTicket.vue
@@ -402,13 +402,28 @@ export default {
       printWindow.document.write("</html>");
       printWindow.document.close();
       printWindow.focus();
-      printWindow.onload = function () {
-        setTimeout(function () {
-          // wait until all resources loaded
-          printWindow.print(); // change window to winPrint
-          printWindow.close(); // change window to winPrint
-        }, 500);
-      };
+      // if (window.print) {
+      //   printWindow.print();
+      // }
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        printWindow.print(); // change window to winPrint
+        // printWindow.close(); // change window to winPrint
+      } else if (is_chrome) {
+        printWindow.onload = function () {
+          setTimeout(function () {
+            // wait until all resources loaded
+            printWindow.print(); // change window to winPrint
+            printWindow.close(); // change window to winPrint
+          }, 500);
+        };
+      } else {
+        printWindow.print();
+        printWindow.close();
+      }
     },
     ...mapActions("guarantedseat", ["cancelTicketAction"]),
     cancelTicket(ticketId) {
