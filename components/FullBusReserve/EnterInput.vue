7c6489d7f2e3473a682177e33ea63b5fbc6db00c
Rijon <rijon.jatri@gmail.com>
2022-12-28 18:00:00 +0600

    FullBus Reservation modal

---
+++ components/FullBusReserve/EnterInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="
+      flex
+      justify-between
+      px-2
+      py-[10px]
+      border border-[#DBDBDB]
+      rounded
+      mt-[10px]
+      w-full
+    "
+  >
+    <input
+      class="
+        h-full
+        w-full
+        focus:outline-0
+        text-xs
+        focus:appearance-none
+        placeholder:text-blackSecondery
+        text-blackPrimary
+      "
+      type="number"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: ["placeholder"],
+};
+</script>
---
+++ components/FullBusReserve/SelectDate.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="
+      flex
+      justify-between
+      px-2
+      py-[10px]
+      border border-[#DBDBDB]
+      rounded
+      mt-[10px]
+      w-full
+    "
+  >
+    <p
+      class="
+        lg:text-xs
+        text-sm
+        2xl:text-base
+        font-normal
+        text-blackSecondery text-left
+      "
+    >
+      Select a date
+    </p>
+    <img
+      src="@/assets/images/home/calendar.svg"
+      alt="arrow-down"
+      class="w-6 h-6 cursor-pointer"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: ["label"],
+};
+</script>
