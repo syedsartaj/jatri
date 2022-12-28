87211b2f2174e02b74893b4d0aa9dc875fd523c1
Rijon <rijon.jatri@gmail.com>
2022-12-28 18:00:21 +0600

    FullBus Reservation modal

---
+++ components/Modal/FullBusReserveModal.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="relative z-[999999999]">
+    <div
+      class="fixed inset-0 bg-blackPrimary bg-opacity-50 transition-opacity"
+    ></div>
+
+    <div class="fixed inset-0 z-10">
+      <div
+        class="
+          flex
+          min-h-full
+          items-end
+          justify-center
+          lg:p-4
+          text-center
+          lg:items-center lg:px-[100px]
+        "
+      >
+        <div
+          class="
+            relative
+            transform
+            overflow-hidden
+            lg:rounded-lg
+            bg-white
+            text-left
+            shadow-xl
+            transition-all
+            w-full
+          "
+        >
+          <div class="bg-white p-4 lg:p-6 w-full">
+            <div class="flex justify-between">
+              <h2 class="text-sm lg:text-xl font-medium text-blackPrimary">
+                Full bus reserve
+              </h2>
+
+              <button @click="() => handlePointPolicyModal()" class="pr-[6px]">
+                <img src="@/assets/images/icons/closeIcon.svg" alt="" />
+              </button>
+            </div>
+
+            <div class="bg-white overflow-y-auto font-normal pt-[10px]">
+              <p class="text-xl text-[#4D4D4F]">
+                Now you can reserve a full bus according to your need. Fill the
+                form and submit. We will check and reserve a bus for you.
+              </p>
+              <div class="border border-[#DBDBDB] rounded h-[598px] mt-6 p-6">
+                <!-- First Row -->
+                <div
+                  class="flex justify-evenly gap-6 mb-6 flex-col lg:flex-row"
+                >
+                  <!-- First Item -->
+                  <div class="flex justify-evenly gap-6 w-full">
+                    <div class="w-full">
+                      <CommonInputLabel label="Journey date" />
+                      <SelectDate />
+                    </div>
+                    <div class="w-full">
+                      <CommonInputLabel label="Return date" />
+                      <SelectDate />
+                    </div>
+                  </div>
+                  <div class="flex justify-evenly gap-6 w-full">
+                    <div class="w-full">
+                      <CommonInputLabel label="Boarding place" />
+                      <ChooseOption label="Choose a location" />
+                    </div>
+                    <div class="w-full">
+                      <CommonInputLabel label="Dropping place" />
+                      <ChooseOption label="Choose a location" />
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Second Row -->
+                <div
+                  class="flex justify-evenly gap-6 mb-6 flex-col lg:flex-row"
+                >
+                  <!-- First Item -->
+                  <div class="flex justify-evenly gap-6 w-full">
+                    <div class="w-full">
+                      <CommonInputLabel label="No. of bus needed" />
+                      <ChooseOption label="Select no. of bus" />
+                    </div>
+                    <div class="w-full">
+                      <CommonInputLabel label="Bus type" />
+                      <ChooseOption label="Select bus type" />
+                    </div>
+                  </div>
+                  <!-- SEcond Item -->
+                  <div class="flex justify-evenly gap-6 w-full">
+                    <div class="w-full">
+                      <CommonInputLabel label="No. of seats" />
+                      <ChooseOption label="Select no. of seats" />
+                    </div>
+                    <div class="w-full">
+                      <CommonInputLabel label="Preferred bus (if any)" />
+                      <ChooseOption label="Select a bus operator" />
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Third Row -->
+                <div class="flex justify-evenly gap-6 flex-col lg:flex-row">
+                  <!-- First Item -->
+                  <div class="flex justify-evenly gap-6 w-full">
+                    <div class="w-full">
+                      <CommonInputLabel label="Approximate budget" />
+                      <EnterInput placeholder="Enter budget amount" />
+                    </div>
+                    <div class="w-full">
+                      <CommonInputLabel label="Comment" />
+                      <EnterInput placeholder="Enter your comment" />
+                    </div>
+                  </div>
+
+                  <!-- Second Item -->
+                  <div class="flex justify-evenly gap-6 w-full"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["close", "selectedTab", "handlePointPolicyModal", "setCurrentTab"],
+  data() {
+    return {};
+  },
+  computed: {},
+};
+</script>
