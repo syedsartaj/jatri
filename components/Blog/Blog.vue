7bf24fcbfec29e4b4a4b24ecdc0955b38af402e0
Rijon <rijon.jatri@gmail.com>
2023-02-13 12:26:25 +0600

    New hooper slider added for blog

--- components/Blog/Blog.vue
+++ components/Blog/Blog.vue
@@ -24,7 +24,7 @@
       </h2>
       <div class="flex justify-between gap-x-4">
         <button
-          @click="scrollLeft"
+          @click="prevSlide"
           class="
             rounded-full
             border
@@ -45,7 +45,7 @@
           />
         </button>
         <button
-          @click="scrollRight"
+          @click="nextSlide"
           class="
             rounded-full
             border
@@ -68,154 +68,154 @@
       </div>
     </div>
 
-    <div
-      class="mt-10 w-full h-[325px] sm:h-[422px] pl-4 lg:pl-0 overflow-hidden"
-    >
-      <VueSlickCarousel v-bind="settings" ref="videoBlogCarousel">
-        <a
+    <div class="mt-10 w-full h-[325px] sm:h-[422px] lg:pl-0 overflow-hidden">
+      <hooper ref="hooper" :settings="hooperSettings">
+        <slide
           v-for="(blog, index) in getBlogList"
           :key="index"
-          class="
-            flex flex-col
-            items-center
-            cursor-pointer
-            w-[320px]
-            sm:w-[350px]
-            xl:w-[392px]
-          "
-          :href="blog.link"
-          style="width: undefine"
+          class="w-[320px] sm:w-[350px] xl:w-[392px]"
         >
-          <img
-            :id="index"
-            :src="imageUrl + blog.image"
-            alt=""
-            class="
-              rounded-t-2xl
-              w-[320px]
-              sm:w-[350px]
-              xl:w-[392px]
-              h-[181px]
-              lg:h-[222px]
-            "
-          />
-          <div
-            class="
-              w-full
-              flex flex-col
-              items-left
-              w-[320px]
-              sm:w-[350px]
-              xl:w-[392px]
-              h-[144px]
-              lg:h-[196px]
-              bg-[#F7F7F7]
-              p-4
-              rounded-b-2xl
-            "
+          <a
+            class="flex flex-col items-center cursor-pointer"
+            :href="blog.link"
           >
-            <p
+            <img
+              :id="index"
+              :src="imageUrl + blog.image"
+              alt=""
               class="
-                lg:mt-4
-                mb-2
-                font-medium
-                text-base
-                lg:text-2xl
-                font-blackPrimary
+                rounded-t-2xl
+                w-[320px]
+                sm:w-[350px]
+                xl:w-[392px]
+                h-[181px]
+                lg:h-[222px]
               "
-            >
-              {{ blog.title }}
-            </p>
-            <p
-              class="
-                text-sm
-                font-normal
-                lg:text-base
-                line-clamp
-                font-blackLight
-                w-[288px]
-                sm:w-[318px]
-              "
-            >
-              {{ blog.description }}
-            </p>
+            />
             <div
               class="
                 w-full
-                hidden
-                lg:flex
-                flex-row
+                flex flex-col
                 items-left
-                gap-x-[10px]
-                mt-[10px]
+                w-[320px]
+                sm:w-[350px]
+                xl:w-[392px]
+                h-[144px]
+                lg:h-[196px]
+                bg-[#F7F7F7]
+                p-4
+                rounded-b-2xl
               "
             >
-              <p class="font-medium text-xl text-corporate">Learn more</p>
-              <img
-                src="@/assets/images/home/arrowRightCorporate.svg"
-                alt=""
-                class="w-6 h-6"
-              />
-            </div>
-          </div>
-        </a>
-      </VueSlickCarousel>
+              <p
+                class="
+                  lg:mt-4
+                  mb-2
+                  font-medium
+                  text-base
+                  lg:text-2xl
+                  font-blackPrimary
+                "
+              >
+                {{ blog.title }}
+              </p>
+              <p
+                class="
+                  text-sm
+                  font-normal
+                  lg:text-base
+                  line-clamp
+                  font-blackLight
+                  w-[288px]
+                  sm:w-[318px]
+                "
+              >
+                {{ blog.description }}
+              </p>
+              <div
+                class="
+                  w-full
+                  hidden
+                  lg:flex
+                  flex-row
+                  items-left
+                  gap-x-[10px]
+                  mt-[10px]
+                "
+              >
+                <p class="font-medium text-xl text-corporate">Learn more</p>
+                <img
+                  src="@/assets/images/home/arrowRightCorporate.svg"
+                  alt=""
+                  class="w-6 h-6"
+                />
+              </div>
+            </div> </a
+        ></slide>
+      </hooper>
     </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
-import VueSlickCarousel from "vue-slick-carousel";
-import "vue-slick-carousel/dist/vue-slick-carousel.css";
-import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+import { Hooper, Slide } from "hooper";
+import "hooper/dist/hooper.css";
 
 export default {
   name: "Blog",
-  components: { VueSlickCarousel },
+  components: { Hooper, Slide },
   data() {
     return {
       slideLeft: false,
       slideRight: false,
       imageUrl: "",
-      settings: {
-        arrows: false,
-        dots: false,
-        autoplay: false,
-        centerMode: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplaySpeed: 5000,
-        speed: 2000,
-        responsive: [
-          {
-            breakpoint: 1522,
-            settings: {
-              slidesToShow: 2,
-              initialSlide: 0,
-            },
+      hooperSettings: {
+        infiniteScroll: true,
+        centerMode: true,
+        autoPlay: false,
+        playSpeed: 3000,
+        itemsToShow: 5,
+        breakpoints: {
+          2300: {
+            itemsToShow: 5,
           },
-          {
-            breakpoint: 1333,
-            settings: {
-              slidesToShow: 1,
-              initialSlide: 0,
-            },
+          2100: {
+            itemsToShow: 4.5,
           },
-        ],
-        rows: 1,
+          1900: {
+            itemsToShow: 4,
+          },
+          1600: {
+            itemsToShow: 3.5,
+          },
+          1440: {
+            itemsToShow: 3,
+          },
+          1024: {
+            itemsToShow: 2.2,
+          },
+          768: {
+            itemsToShow: 2,
+          },
+          425: {
+            itemsToShow: 1.2,
+          },
+          0: {
+            itemsToShow: 1,
+          },
+        },
       },
     };
   },
   methods: {
-    scrollLeft() {
-      this.$refs.videoBlogCarousel.prev();
+    prevSlide() {
+      this.$refs.hooper.slidePrev();
       this.slideLeft = true;
       this.slideRight = false;
     },
-    scrollRight() {
-      this.$refs.videoBlogCarousel.next();
+    nextSlide() {
+      this.$refs.hooper.slideNext();
       this.slideRight = true;
       this.slideLeft = false;
     },
