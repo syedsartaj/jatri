4c1c4bbab3899d0807ca52517c55ae304c5f81d2
Rijon <44047172+Rijon1810@users.noreply.github.com>
2023-05-15 14:14:58 +0600

    Spelling mistake issue fix

--- components/buslist/GuaranteedSeatList.vue
+++ components/buslist/GuaranteedSeatList.vue
@@ -17,7 +17,7 @@
       <div class="h-[500px] flex justify-center items-center bg-[#f7f7f7]">
         <!-- <TripNotFound/> -->
         <OpssAlert
-          :details="'Looks llike we could not find any trip according to your search. Try different route or date and search again.'"
+          :details="'Looks like we could not find any trip according to your search. Try different route or date and search again.'"
           :customStyle="'px-[92.5px]'"
         />
       </div>
