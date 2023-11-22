c28c79e7794c071ccddd046f9a3f27a59b459c38
Rijon <rijon.stack@gmail.com>
2023-11-22 17:31:39 +0600

    Bug fix

---
+++ components/Trip/AvailableTripSection/launch/LaunchSingleTrip/SeatAvailabilityButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="w-full gap-x-[4px] flex flex-row items-center cursor-pointer"
+    @click="handleSeatAvailableModal"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+    >
+      <path
+        d="M1.99902 9V10.5H3.49902V9H8.49903V10.5H9.99903V7.5H1.99902V9ZM9.49903 5H10.999V6.5H9.49903V5ZM0.999023 5H2.49902V6.5H0.999023V5ZM8.49903 6.5H3.49902V2.5C3.49902 1.95 3.94902 1.5 4.49902 1.5H7.49902C8.04903 1.5 8.49903 1.95 8.49903 2.5V6.5Z"
+        fill="#48A43F"
+      />
+    </svg>
+
+    <p class="text-xs font-medium text-[#48A43F]">Seat Availability</p>
+
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+    >
+      <path
+        d="M6.53042 5.99987L4.05542 3.52487L4.76242 2.81787L7.94442 5.99987L4.76242 9.18187L4.05542 8.47487L6.53042 5.99987Z"
+        fill="#48A43F"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["handleSeatAvailableModal"],
+  name: "SeatAvailabilityButton",
+};
+</script>
--- components/paymentMethod/GateWayOption.vue
+++ components/paymentMethod/GateWayOption.vue
@@ -1,35 +1,57 @@
 <template>
-     <button 
-          class="flex justify-between items-center w-1/2 rounded-lg px-5 py-[15px] border" 
-          :class="activePlan === planName ? 'bg-[#EFF7FD] border-[#1E88E5]' : 'bg-[#f7f7f7] border-[#f7f7f7]'"
-          @click='updateActivePlan' 
-          @keydown.space='updateActivePlan' 
-     >
-          <div class="flex flex-wrap justify-start items-center gap-x-3">
-               <div class="grid grid-cols-4 lg:grid-cols-2 gap-x-[5.57px] lg:gap-x-3 lg:gap-y-[7px] order-last lg:order-first">
-                    <img src="@/assets/images/amex-logo.svg" alt="amex" class="w-5">
-                    <img src="@/assets/images/mastercard-logo.svg" alt="mastercard" class="w-[18px]">
-                    <img src="@/assets/images/visa-logo.svg" alt="visa" class="w-[21px]">
-                    <img src="@/assets/images/bkash-logo.svg" alt="bkash" class="w-[27px]">
-               </div>
-               <div class="bg-[#D9D9D9] w-[1px] h-9 hidden lg:block"></div>
-               <img src="@/assets/images/sslcommerz-logo.svg" alt="" class="mb-[6px] lg:mb-0">
-          </div>
-          <span class=''>{{ planDiscount }}</span>
-          <img v-if='activePlan === planName' src="@/assets/images/icons/tik-blue.svg" alt="tik">
-     </button>
+  <button
+    class="flex justify-between items-center w-1/2 rounded-lg px-4 border h-[60px]"
+    :class="
+      activePlan === planName
+        ? 'bg-[#EFF7FD] border-[#1E88E5]'
+        : 'bg-[#f7f7f7] border-[#f7f7f7]'
+    "
+    @click="updateActivePlan"
+    @keydown.space="updateActivePlan"
+  >
+    <div class="flex flex-wrap justify-start items-center gap-x-3">
+      <div
+        class="grid grid-cols-4 lg:grid-cols-2 gap-x-[5.57px] lg:gap-x-3 lg:gap-y-[7px] order-last lg:order-first"
+      >
+        <img src="@/assets/images/amex-logo.svg" alt="amex" class="w-5" />
+        <img
+          src="@/assets/images/mastercard-logo.svg"
+          alt="mastercard"
+          class="w-[18px]"
+        />
+        <img src="@/assets/images/visa-logo.svg" alt="visa" class="w-[21px]" />
+        <img
+          src="@/assets/images/bkash-logo.svg"
+          alt="bkash"
+          class="w-[27px]"
+        />
+      </div>
+      <div class="bg-[#D9D9D9] w-[1px] h-9 hidden lg:block"></div>
+      <img
+        src="@/assets/images/sslcommerz-logo.svg"
+        alt=""
+        class="mb-[6px] lg:mb-0"
+      />
+    </div>
+    <span class="">{{ planDiscount }}</span>
+    <img
+      v-if="activePlan === planName"
+      src="@/assets/images/icons/tik-blue.svg"
+      alt="tik"
+    />
+  </button>
 </template>
 <script>
 export default {
-     model: {
-          prop: 'activePlan',
-          event: 'onUpdatePlan'
-     },
-     props: ['planName', 'planIcon', 'planDiscount', 'activePlan'],
-     methods: {
-          updateActivePlan () {
-               this.$emit('onUpdatePlan', this.planName);
-          }
-     }
+  model: {
+    prop: "activePlan",
+    event: "onUpdatePlan",
+  },
+  props: ["planName", "planIcon", "planDiscount", "activePlan"],
+  methods: {
+    updateActivePlan() {
+      this.$emit("onUpdatePlan", this.planName);
+    },
+  },
 };
 </script>
