59256446786a8e8a103fae7506f9213d25b56b04
masud-jatri <masud.jatri@gmail.com>
2024-09-15 13:01:00 +0600

    feat: separated banner component for bus and launch and  made minor modification to classes.

---
+++ components/BannerSection/launch/LaunchBannerLarge.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hidden lg:block relative homeBanner-launch">
+    <div
+      class="absolute lg:top-[80px] lg:left-[96px] lg:right-[96px] z-40 flex flex-col"
+    >
+      <div class="ml-[4px]">
+        <div
+          class="text-[28px] md:text-[45px] lg:text-[45px] text-white font-bold"
+        >
+          Reserve your launch tickets online
+        </div>
+        <div class="text-xl text-white font-medium">
+          Buy tickets online hassle-free.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.homeBanner-launch {
+  background-image: linear-gradient(
+      90deg,
+      rgba(21, 20, 20, 0.57) 0.01%,
+      rgba(255, 255, 255, 0) 99.64%
+    ),
+    url("@/assets/images/home/bannerImageLaunchWeb.png");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+
+@media only screen and (min-width: 992px) {
+  .homeBanner-launch {
+    height: 400px;
+  }
+}
+
+@media only screen and (min-width: 1024px) {
+  .homeBanner-launch {
+    height: 286px;
+  }
+}
+</style>
